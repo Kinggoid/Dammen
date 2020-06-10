dfcad99279155d0009d61db31247672f5548b3a9
Kinggoid <thijmespambox@gmail.com>
2020-06-10 22:18:38 +0200

    11e commit. je kan nu het spel eindigen door alle stukken van de ander af te pakken en als je iemand hebt geslagen en je kan nog iemand slaan, dan moet dat (ik forceer je niet het pad te kiezen waar je uiteindelijk de meeste stenen pakt).

--- Dammen/Classes.py
+++ Dammen/Classes.py
@@ -108,10 +108,53 @@ def damPakken(board, stuk, x, y):
     return False
 
 
+def nogEenKeerSpringen(board, stuk):
+    positie = stuk.positie
+    team = stuk.team
+
+    x_sprongen = ['positie[0] + 1', 'positie[0] - 1']
+    y_sprongen = ['positie[1] + 1', 'positie[1] - 1']
+
+    for i in x_sprongen:
+        for j in y_sprongen:
+            x = eval(i)
+            y = eval(j)
+            if 7 >= x >= 0 and 7 >= y >= 0:
+                veld = board[y][x]
+                if veld != 0:
+                    if veld.team != team:
+                        sprong_x = (x - positie[0]) + x
+                        sprong_y = (y - positie[1]) + y
+                        if 7 >= sprong_x >= 0 and 7 >= sprong_y >= 0:
+                            if board[sprong_y][sprong_x] == 0:
+                                return True
+    return False
+
+
+def draaiDeBeurt(beurt):
+    if beurt:
+        return False
+    else:
+        return True
+
+
+def einde(wit, zwart):
+    if wit == 0 or zwart == 0:
+        if wit == 0:
+            print('Zwart wint')
+        else:
+            print('Wit wint')
+        return True
+    else:
+        return False
+
 
 def inner_loop():
     pygame.init()
     stukken(board)
+    aantalWitteStukken = 2
+    aantalZwarteStukken = 2
+    alleenSprong = False
     afmetingen = [900, 900]
     breedte = afmetingen[0] // 8
     hoogte = afmetingen[1] // 8
@@ -124,18 +167,20 @@ def inner_loop():
     damsteen.radius = afmetingen[0] // 20
     damsteen.rand = afmetingen[1] // 150
 
-    beurt = 1
+    beurt = True
 
     game_over = 0
     while game_over == 0:
         for event in pygame.event.get():
-            mouse_pos = pygame.mouse.get_pos()
-            mouse_matrix_pos = mouse_pos[0] // breedte, mouse_pos[1] // hoogte
 
             clock = pygame.time.Clock()
 
             if event.type == pygame.QUIT:
                 game_over = True
+
+            if einde(aantalWitteStukken, aantalZwarteStukken):
+                game_over = True
+                break
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 print('hoi')
@@ -144,9 +189,14 @@ def inner_loop():
                 old_y = pos[1] // hoogte
                 welkVak = board[old_y][old_x]
 
+                if welkVak == 0:
+                    break
+
                 if beurt % 2 == welkVak.team:
                     while True:
                         event = pygame.event.wait()
+
+
                         if event.type == pygame.QUIT:
                             break
 
@@ -157,15 +207,17 @@ def inner_loop():
                             friendly = checkIfFriendly(board, new_x, new_y)
 
                             if friendly:
-                                if damZetten(board, welkVak, new_x, new_y):
+                                if not alleenSprong and damZetten(board, welkVak, new_x, new_y):
+
                                     welkVak.positie[0] = new_x
                                     welkVak.positie[1] = new_y
 
                                     board[new_y][new_x], board[old_y][old_x] = welkVak, 0
 
-                                    beurt += 1
+                                    beurt = draaiDeBeurt(beurt)
                                     break
                                 elif damPakken(board, welkVak, new_x, new_y):
+                                    print('ben hier')
                                     board[(new_y + welkVak.positie[1]) // 2][(new_x + welkVak.positie[0]) // 2] = 0
 
                                     welkVak.positie[0] = new_x
@@ -173,7 +225,19 @@ def inner_loop():
 
                                     board[new_y][new_x], board[old_y][old_x] = welkVak, 0
 
-                                    beurt += 1
+                                    if welkVak.team:
+                                        aantalZwarteStukken -= 1
+                                    else:
+                                        aantalWitteStukken -= 1
+
+
+                                    if nogEenKeerSpringen(board, welkVak):
+                                        alleenSprong = True
+                                        print('in het hol van de leeuw')
+                                        break
+
+                                    alleenSprong = False
+                                    beurt = draaiDeBeurt(beurt)
                                     break
 
             clock.tick(10)
